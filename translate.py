ccee6174796ebf9f2e9c20bc065d4281e704507b
Andrew Gambardella <gambs@robots.ox.ac.uk>
2021-04-26 00:18:12 +0100

    remove master files

--- translate.py
+++ translate.py
@@ -8,6 +8,57 @@ import csv
 name_dict = {
 }
 
+def sort_func(el):
+    ret = 0
+    remaining = el.split('/')[1]
+    chap = int(remaining[:2])
+    ret += chap*10000
+    remaining = remaining[2:]
+    remaining = remaining.split('_')[-1]
+    remaining = remaining.split('.')[0]
+    try:
+
+    1/0
+
+
+def get_fnames():
+    ret = []
+    for root, dir, file in os.walk('txt_scripts_jp'):
+        for fname in file:
+            ret.append(os.path.join(root, fname))
+    return ret
+
+def create_translation_csv(outname=''):
+    total_lines = []
+    fnames = get_fnames()
+    fnames = sorted(fnames, key=sort_func)
+    print(fnames)
+    1/0
+    for fname in fnames:
+        with open(os.path.join('script', fname)) as f:
+            scriptline = f.readline()
+            while scriptline:
+                scriptline = f.readline()
+                if 'ruby' in scriptline:
+                    next_line = f.readline()
+                    endruby = f.readline()
+                    next_next_line = f.readline()
+                    scriptline = next_line[:-3] + next_next_line[1:]
+                m = re.search(r'^"(.*)",$', scriptline)
+                try:
+                    total_lines.append(m.group(1))
+                except:
+                    continue
+    unique_set = set()
+    unique_add = unique_set.add
+    unique_lines = [x for x in total_lines if not (x in unique_set or unique_add(x))]
+    print('Total number of lines: {}, Number of unique lines: {}'.format(len(total_lines), len(unique_lines)))
+
+    with open(outname, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for line in unique_lines:
+            # print(line)
+            csvwriter.writerow([line, line])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
